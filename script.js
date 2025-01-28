61de8ddfabf9afda9679afd0d4d619961ab8d5e4
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-01-28 16:07:56 +0530

    Update scroll behavior for "Back to Top" button and refine smooth scroll function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Show "Back to Top" button on scroll
 window.onscroll = function() {
   const backToTopButton = document.querySelector(".back-to-top");
-  if (document.documentElement.scrollTop > 200) {
+  if (document.documentElement.scrollTop > 300) {
     backToTopButton.classList.add("show");
   } else {
     backToTopButton.classList.remove("show");
@@ -13,7 +13,7 @@ function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
-// Smooth Scroll to Project Section
+// Smooth Scroll to Section
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
   section.scrollIntoView({ behavior: 'smooth' });
