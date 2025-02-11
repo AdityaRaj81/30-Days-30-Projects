4c9f1a320e92211de67f395250797a18f047c939
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-02-11 13:12:10 +0530

    Add Pop Up Info feature with HTML, CSS, and JavaScript

---
+++ 15_Popup_info/script.js
@@ -0,0 +1,9 @@
+let popUp=document.getElementById("pop-up");
+
+function openPopUp(){
+    popUp.classList.add("open-pop-up");
+}
+
+function closePopUp(){
+    popUp.classList.remove("open-pop-up");
+}
