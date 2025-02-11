dcc376c1bcd0e1929c540f30d507cd33887c50e8
AdityaRaj81 <Connect2RajAditya@gmail.com>
2025-02-11 13:13:15 +0530

    Add Image Transition feature with HTML, CSS, and JavaScript

---
+++ Image_transition/script.js
@@ -0,0 +1,13 @@
+var ImgBox = document.querySelector(".img-box");
+var ImgWrap = document.querySelector(".img-wrap");
+var originalImg = document.getElementById("originalImg");
+var line = document.getElementById("line");
+
+
+originalImg.style.width = ImgBox.offsetWidth + "px";
+var leftSpace = ImgBox.offsetLeft
+ImgBox.onmousemove = function(e){
+    var boxWidth = (e.pageX - leftSpace) + "px";
+    ImgWrap.style.width = boxWidth;
+    line.style.left = boxWidth;
+}
